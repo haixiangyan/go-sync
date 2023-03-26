1a8993a764c7df7eadbc4958c3ddd29e9d1d5549
haixiangyan <haixiang6123@gmail.com>
2023-03-26 23:25:39 +0800

    feat: 实现文件上传

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"embed"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/zserge/lorca"
 	"io/fs"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"path"
+	"path/filepath"
 	"strings"
 	"syscall"
 )
@@ -28,6 +31,9 @@ func main() {
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
 		// 静态文件前缀处理
 		router.StaticFS("/static", http.FS(staticFiles))
+
+		// 主路由
+		router.POST("/api/v1/texts", TextsController)
 
 		// 404
 		router.NoRoute(func(c *gin.Context) {
@@ -69,5 +75,44 @@ func main() {
 	select {
 	case <-ui.Done():
 	case <-chSignal:
+	}
+}
+
+func TextsController(c *gin.Context) {
+	var json struct {
+		Raw string `json:"raw"`
+	}
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		// 当前执行文件的路径
+		exe, err := os.Executable()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// 创建 /uploads 目录
+		dir := filepath.Dir(exe)
+		uploads := filepath.Join(dir, "uploads")
+		err = os.MkdirAll(uploads, os.ModePerm)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// 生成随机路径
+		filename := uuid.New().String()
+		fullpath := path.Join("uploads", filename+".txt")
+
+		// 写文件
+		err = os.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
 	}
 }
